688ce0940ae985707ba09b7511f2c39402c677f0
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-12-29 17:38:18 -0300

    Adicionando responsividade

--- script.js
+++ script.js
@@ -1,37 +1,69 @@
-function analyze() {
-  if (document.querySelector('table')) document.querySelector('table').remove();
-  let text = document.querySelector('.pasteText').value;
-  let rows = text.split('\n').map( (str) => str.split(' '));
+const getUserInput = () => document.querySelector(".pasteText").value;
+const printInput = input =>
+  (document.getElementById("output").innerHTML = input);
+  
+const clearInput = str =>
+  str
+    .replace(/[?.!,"\(\)]/g, "")
+    .replace(/[ ]{2,}/g, "")
+    .trim()
+    .toLowerCase();
 
-  function concat_array(array) {
-    return array.reduce( (new_array, item) => new_array.concat( Array.isArray(item) ? concat_array(item) : item), []);
-  }
+const wordFrequency = string =>
+  string
+    .split(/\s/)
+    .reduce(
+      (output, word) =>
+        Object.assign(output, { [word]: output[word] ? output[word] + 1 : 1 }),
+      {}
+    );
 
-  document.querySelector('.countWords').insertAdjacentHTML('afterend', `
-    <table>
-      <tr>
-        <th>Palavra</th>
-        <th>Quantidade</th>
-      </tr>
-    </table>`)
+const sortByValue = obj =>
+  Object.entries(obj)
+    .map(currentValue => [currentValue[1], currentValue[0]])
+    .sort((a, b) => parseInt(b) - parseInt(a))
+    .map((currentValue, index) => [
+      index + 1,
+      currentValue[0],
+      currentValue[1]
+    ]);
 
-  let words = concat_array(rows);
-  let map = new Map();
-  for (let word of words) {
-    let n_word = word.replace(/[?.!,"\(\)]/g, "").replace(/[ ]{2,}/g, "").trim().toLowerCase();
-    if (!(map.has(n_word))) map.set(n_word, 1);
-    else {
-      let count = map.get(n_word);
-      ++count;
-      map.set(n_word, count);
-    }
-  }
+const divFrequency = "wordTable";
+const headerFrequency = ["Rank", "Count", "Word"];
 
-  for (let [word, count] of Array.from(map.entries()).sort( (a, b) => b[1]-a[1])) {
-    document.querySelector('table').insertAdjacentHTML('beforeend', `
-    <tr>
-      <td>${word}</td>
-      <td>${count}</td>
-    </tr>`);
-  }
-}
+const addTable = (divId, headers, data) => {
+  const wordTable = document.getElementById(divId);
+  const table = document.createElement("table");
+
+  //TABLE Headers
+  const tr = document.createElement("tr");
+  table.appendChild(tr);
+  headers.forEach(currentValue => {
+    const th = document.createElement("th");
+    th.appendChild(document.createTextNode(currentValue));
+    tr.appendChild(th);
+  });
+
+  //TABLE ROWS
+  data.forEach(currentValue => {
+    const tr = document.createElement("tr");
+    currentValue.forEach(currentValue => {
+      const td = document.createElement("td");
+      td.appendChild(document.createTextNode(currentValue));
+      tr.appendChild(td);
+    });
+    table.appendChild(tr);
+  });
+
+  wordTable.appendChild(table);
+};
+
+const processData = () => {
+  const sortedFrequency = sortByValue(wordFrequency(clearInput(getUserInput())));
+  document.getElementById("wordTable").innerHTML = "";
+  addTable(divFrequency, headerFrequency, sortedFrequency);
+};
+
+$(document).ready(function () {
+  $("#countWords").on("click", processData);
+});
